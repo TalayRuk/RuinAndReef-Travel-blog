06cd682f48c0c10a40ea2f3dcdd8935693b53add
vp <notejde@gmail.com>
2016-11-16 20:48:08 -0800

    add up to view/home/index

---
+++ src/RuinAndReef/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace RuinAndReef
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //1 add the MVC service to the project.
+            //The ConfigureServices method is where we configure famework services to our application
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //2 - UseMvc(...) - telling our app that it'll be using the MVC framework
+            //Then the Configure method is where we tell ASP.NET what frameworks we want to use in our app
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                //this also set up default routing which tells the project to use the index action of the Home Controller as the default route,
+                // and if we have any parameters will be passed as id 
+                //Next we'll create Controllers/HomeController.cs
+            });
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello Note!");
+            });
+        }
+    }
+}
